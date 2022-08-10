20ff09ff2138288b081436f47d91ba1e13b5ad36
Jose Alexis Correa valencia <jalexiscv@gmail.com>
2022-08-10 15:45:42 -0500

    Update tables.php

--- tables.php
+++ tables.php
@@ -1,14 +1,32 @@
+<?php
 /*
 * -----------------------------------------------------------------------------
 * [Build]
 * -----------------------------------------------------------------------------
 */
-$smarty = service('smarty');
-$smarty->set_Mode('bs5x');
-$smarty->assign('type', 'table');
-$smarty->assign('header', sprintf(lang('Disa.recommendations-priority-list-title'), $oid));
-$smarty->assign('body', '');
-$smarty->assign('table', $table);
-$smarty->assign('footer', null);
-$smarty->assign('file', __FILE__);
-echo($smarty->view('components/tables/index.tpl'));
+$scard = service("smarty");
+$scard->caching = 0;
+$scard->set_Mode("bs5x");
+$scard->assign("type", "normal");
+$scard->assign("header", false);
+$scard->assign("header_back", false);
+$scard->assign("class", "mb-2");
+$scard->assign("body",$f.$table );
+$scard->assign("footer", null);
+$scard->assign("file", __FILE__);
+echo($scard->view('components/cards/index.tpl'));
+?>
+
+<?php
+$stable = service('smarty');
+$stable->caching = 0;
+$stable->set_Mode('bs5x');
+$stable->assign('type', 'table');
+$stable->assign('header', lang('Characterize.options-list-title'));
+$stable->assign('header_add', "/characterize/options/create/{$oid}");
+$stable->assign('body', '');
+$stable->assign('table', $table);
+$stable->assign('footer', null);
+$stable->assign('file', __FILE__);
+echo($stable->view('components/tables/index.tpl'));
+?>
